06eec33a81826d900bbbccc2bde8edffb808441e
markhinho <mark_ghabrial@outlook.com>
2017-05-15 20:57:32 -0700

    day01

---
+++ day01/js/script.js
@@ -0,0 +1,62 @@
+// ES6 - ECMAScript6
+
+//good
+var lowercase;
+var camelCase;
+var use_underscores;
+var _underscore;
+var $dollarsign;
+
+//meh
+var SuperCamelCase;
+
+//bad
+// var no spaces;
+// var no-hyphens;
+// var no numbers;
+
+console.log('Sending a message to the console');
+console.info('sending information message');
+console.warn('here is a warning');
+console.error('error');
+
+
+
+//single line comment
+/*
+  multi
+  line
+  comments
+*/
+
+var
+
+
+                      freedom
+
+
+
+
+
+
+        =
+
+
+
+        "a string of javascript";
+
+console.log(freedom);
+
+
+// template string
+
+var javascriptTemplateString = `
+and
+type
+whatever
+you
+want
+:)
+`;
+
+console.log(javascriptTemplateString);
